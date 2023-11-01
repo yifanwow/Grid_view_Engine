7c754ac2ae22388e4be30b4029f928911921c3ed
yifanwow <yifanovo@gmail.com>
2023-11-01 10:29:22 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+A self-build software which can allow user to change the Grid on their Steam library.
--- obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -76,7 +76,7 @@ namespace Grid_view_Engine {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Grid_view_Engine;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Grid_view_Engine;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
